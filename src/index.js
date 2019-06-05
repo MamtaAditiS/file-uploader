e1cd461df64e53d06ac141c076319973548f8cea
MamtaAditiS <mamtasingla007@gmail.com>
2019-06-05 22:16:12 +0530

    initial set up

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './styles/main.css'
+// main app
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('app'))
